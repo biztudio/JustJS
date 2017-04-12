d9159f3834b9cc2f1287043fbf62f44eaa3b94b9
Yang SHEN <yang.shen@commscope.com>
2017-04-12 23:29:17 +0800

    webpack with vue to practice

---
+++ webpack.npm.practice/webpack.config.js
@@ -0,0 +1,15 @@
+var path = require('path');
+
+module.exports = {
+    devtool: 'source-map',
+    
+    entry:{
+        app:'./js/app.js'
+    },
+
+    output:{
+        filename:'[name].bundle.js',
+         //对应一个绝对路径，此路径是你希望一次性打包的目录
+        path: path.resolve(__dirname, 'dist')
+    }
+};
