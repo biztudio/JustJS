bca6a28be9ae839fc7a8a6b05499e990fbcd39db
Yang SHEN <yang.shen@commscope.com>
2018-01-05 16:08:08 +0800

    add badge in menu

--- ivs/src/components/appbar/appbar.vue
+++ ivs/src/components/appbar/appbar.vue
@@ -1,10 +1,14 @@
 <template>
 <mu-appbar title="Dashboard Gauges">
   <mu-icon-button icon="menu" slot="left"/>
+  
   <mu-icon-menu icon="more_vert" slot="right">
       <mu-menu-item title="Add Gauge"/>
       <mu-menu-item title="Edit Gauge"/>
       <mu-menu-item title="Remove Gauge"/>
+      <mu-menu-item title="Only Warning Gauges">
+           <mu-badge content="3" secondary slot="after"/>
+      </mu-menu-item>    
     </mu-icon-menu>
 </mu-appbar>
 </template>
